e13d5fb99a0f17092706bd9ea7811209c50971c1
SimonSchalk <73887220+SimonSchalk@users.noreply.github.com>
2020-11-11 11:31:03 +0100

    change space

--- test.py
+++ test.py
@@ -8,4 +8,4 @@ Created on Wed Nov 11 10:57:18 2020
 from Schaltjahr import Schaltjahr
 
 def test_Schaltjahr():
-    assert Schaltjahr(2014) == print("ist KEIN Schaltjahr")
+    assert Schaltjahr(2014) == print(" ist KEIN Schaltjahr!")
